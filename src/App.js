79786b4f8904dad9e620ab228b29cd699d53e5bb
Lyutink <krivenko.lyuda@gmail.com>
2022-01-05 12:45:54 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Profile } from "components/Profile/Profile";
+import { TaskList } from "components/TaskList/TaskList";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TaskList />
+    </>
   );
-}
+};
+
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
