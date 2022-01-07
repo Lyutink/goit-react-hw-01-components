155ebbf135cf5e81834467a28dbca446039c075a
Lyutink <krivenko.lyuda@gmail.com>
2022-01-07 14:15:06 +0300

    done

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,10 +3,8 @@ import PropTypes from "prop-types";
 import { FriendStatus, FriendAvatar, FriendName } from "./FriendList.styled";
 
 export default function FriendListItem({ avatar, name, isOnline }) {
-  //   {isOnline && 'Онлайн'}
   return (
     <>
-      {/* <FriendStatus>{isOnline ? "Онлайн" : "Офлайн"}</FriendStatus> */}
       <FriendStatus isOnline={isOnline}></FriendStatus>
       <FriendAvatar src={avatar} alt="User avatar" width="48" />
       <FriendName>{name}</FriendName>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-//import StaticsList from './StaticsList';
+
 import {
   StatisticsSection,
   Title,
